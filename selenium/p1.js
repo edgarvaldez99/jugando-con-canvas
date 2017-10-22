7989d11d4def13493d7e3a533f6b3610974714ac
Edgar <edgarvaldez99@gmail.com>
2017-10-22 01:07:50 -0300

    Modificado p1 de selenium para que trabaje con el login de chrome

--- selenium/p1.js
+++ selenium/p1.js
@@ -4,7 +4,12 @@ let driver = new Builder()
     .forBrowser('firefox')
     .build();
 
-driver.get('http://www.google.com/ncr');
-driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-driver.quit();
+driver.get('https://www.gmail.com');
+driver.findElement(By.name('identifier')).sendKeys('edgarvaldez99@gmail.com', Key.RETURN);
+driver.findElement(By.id('identifierNext')).click();
+
+const passBy = By.name("password");
+driver.wait(until.elementLocated(passBy) , 1000);
+driver.wait(until.elementIsVisible(driver.findElement(passBy)), 10000);
+driver.findElement(passBy).sendKeys('26523697-Ed', Key.RETURN);
+driver.findElement(By.id('passwordNext')).click();
